1f6650657caa94fad8103f9117327eb6c5cf7249
lionkgb <lionceau.lk@gmail.com>
2017-02-17 13:19:08 +0200

    Completed Restful action

--- app/controllers/super_heroes_controller.rb
+++ app/controllers/super_heroes_controller.rb
@@ -7,11 +7,27 @@ class SuperHeroesController < ApplicationController
     @super_hero = Unirest.get("#{ENV['DOMAIN']}/superheroes/#{params[:id]}.json").body
   end 
 
+  def new 
+  end 
+
   def create 
     @super_hero = Unirest.post("#{ENV['DOMAIN']}/superheroes.json", :headers => {"Accept"=> "application/json"}, :parameters => {:name => params[:name], :power => params[:power], :descriptor => params[:descriptor]}).body
   render :show
   end
 
-  def new 
+  def edit 
+    @super_hero = Unirest.get("#{ENV['DOMAIN']}/superheroes/#{params[:id]}.json").body
   end 
+
+  def update 
+    @super_hero = Unirest.patch("#{ENV['DOMAIN']}/superheroes.json", :headers => {"Accept"=> "application/json"}, :parameters => {:name => params[:name], :power => params[:power], :descriptor => params[:descriptor]}).body
+    redirect_to "/superheroes/#{@superhero['id']}"
+  end 
+
+  def destroy 
+    Unirest.delete("#{ENV['DOMAIN']}/superheroes/#{params[:id]}.json").body
+    redirect_to "/"
+  end
+
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
  get "/" => 'super_heroes#index'
  get "/superheroes" => 'super_heroes#new' 
  get "/superheroes/:id", to: 'super_heroes#show'
+ get "/superheroes/:id/edit" => 'super_heroes#edit'
+ patch "/superheroes/:id" => 'super_heroes#update'
+ delete "/superheroes/:id" => 'super_heroes#destroy'
 end
