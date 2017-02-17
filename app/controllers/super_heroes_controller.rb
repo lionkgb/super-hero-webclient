911111f5015b5d0fd745e6f5b24f932e3915806c
lionkgb <lionceau.lk@gmail.com>
2017-02-17 11:56:48 +0200

    Added env variables to DRY code

--- app/controllers/super_heroes_controller.rb
+++ app/controllers/super_heroes_controller.rb
@@ -1,14 +1,14 @@
 class SuperHeroesController < ApplicationController
   def index 
-    @super_heroes = Unirest.get("http://localhost:3000/api/v1/superheroes.json").body
+    @super_heroes = Unirest.get("#{ENV['DOMAIN']}/superheroes.json").body
   end 
 
   def show 
-    @super_hero = Unirest.get("http://localhost:3000/api/v1/superheroes/#{params[:id]}.json").body
+    @super_hero = Unirest.get("#{ENV['DOMAIN']}/superheroes/#{params[:id]}.json").body
   end 
 
   def create 
-    @super_hero = Unirest.post("http://localhost:3000/api/v1/superheroes.json", :headers => {"Accept"=> "application/json"}, :parameters => {:name => params[:name], :power => params[:power], :descriptor => params[:descriptor]}).body
+    @super_hero = Unirest.post("#{ENV['DOMAIN']}/superheroes.json", :headers => {"Accept"=> "application/json"}, :parameters => {:name => params[:name], :power => params[:power], :descriptor => params[:descriptor]}).body
   render :show
   end
 
