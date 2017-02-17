f5bd71fd5ac04745e743fff95d4f52d54fecc9b8
lionkgb <lionceau.lk@gmail.com>
2017-02-17 11:18:19 +0200

    Restful Actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+ post "/superheroes" => 'super_heroes#create'
+ get "/" => 'super_heroes#index'
+ get "/superheroes" => 'super_heroes#new' 
  get "/superheroes/:id", to: 'super_heroes#show'
 end
